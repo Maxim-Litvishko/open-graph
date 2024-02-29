cf2f43a5b3a89f19e6d07ec4da0e08f873054862
EugeneKaluginModsen <eugene.kalugin@modsen-software.com>
2024-02-29 22:25:39 +0300

    wqewqe

--- src/app/api/og/route.tsx
+++ src/app/api/og/route.tsx
@@ -4,6 +4,7 @@ export async function GET() {
   const imgUrl = new URL('../../../public/custom-head2.jpg', import.meta.url).href
   return new ImageResponse(
     (
+      // eslint-disable-next-line @next/next/no-img-element
       <img src={'https://vercel-nu-six-90.vercel.app/header.png'} alt='werw' />
     ),
     {
