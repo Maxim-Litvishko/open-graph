fc8edb901a89c6dacfec22ee70d2c20925e4b47b
EugeneKaluginModsen <eugene.kalugin@modsen-software.com>
2024-02-29 23:50:03 +0300

    weqwe

--- src/pages/catalog/index.tsx
+++ src/pages/catalog/index.tsx
@@ -12,7 +12,7 @@ export default function Page() {
       <meta property="og:description"
         content="Custom software development for your needs. Front-end · Back-end · Mobile Development · DevOps · QA & Testing provided by a reliable tech company." />
       <meta property="og:image"
-        content="https://modsen-software-git-mp-eie9ah-modsen-team.vercel.app/_next/image?url=images/custom-head/opengraph-image.jpg&w=1200&q=75" />
+        content="https://modsen-software-git-mp-eie9ah-modsen-team.vercel.app/_next/image?url=images/custom-head/opengraph-image.webp&w=1200&q=100" />
       <meta property="og:site_name" content="Modsen" />
       <meta property="og:locale" content="en_US" />
       <title>Modsen | Modern Software Engineering</title>
